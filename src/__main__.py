7510667b8b00b80b339c0391853df632307ce4fb
Pasindu Muthumala <lgpmuthumala@gmail.com>
2023-10-19 22:50:38 +0530

    Modify

--- src/__main__.py
+++ src/__main__.py
@@ -1,8 +1,11 @@
 from src.auth.signup import SignUp
-from src.app import main
+from src.main import main
 
 def app():
-    choice = input("Enter 1 to sign in or 2 to sign up: ")
+    choice = input("Enter :"
+                   "\n      1 to signin"
+                   "\n      2 to signup"
+                   "\n")
     if choice == '1':
         main()
     elif choice == '2':
