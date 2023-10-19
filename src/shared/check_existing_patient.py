fdd7061ff7001132cfaae8611b24f9f9397a2e47
Pasindu Muthumala <lgpmuthumala@gmail.com>
2023-10-19 20:42:57 +0530

    Create method for check existing account

---
+++ src/shared/check_existing_patient.py
@@ -0,0 +1,9 @@
+import json
+def check_existing_patients(id):
+    with open("config.json", 'r') as file:
+        data = json.load(file)
+        patients = data['users']
+        for patient in patients:
+            if id == str(patient['id']) and patient['user_type'] == 'patient':
+                return True
+        return False
